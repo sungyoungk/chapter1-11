22f2407c6fe1f8b8c8469affd0c80499d7cd4667
kordobby <poordobby@naver.com>
2022-05-12 16:37:06 +0900

    최종이요

---
+++ app.py
@@ -0,0 +1,176 @@
+from pymongo import MongoClient
+import certifi
+
+import jwt
+import datetime
+import hashlib
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from datetime import datetime, timedelta
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
+
+SECRET_KEY = 'SPARTA'
+
+ca = certifi.where()
+
+client = MongoClient('mongodb+srv://test:sparta@cluster0.rwqgk.mongodb.net/Cluster0?retryWrites=true&w=majority',
+                     tlsCAFile=ca)
+db = client.dbsparta
+
+
+@app.route('/')
+def home():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"username": payload["id"]})
+        return render_template('index.html', user_info=user_info)
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
+
+@app.route('/login')
+def login():
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
+
+
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    # 로그인
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+            'id': username_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    nickname_receive = request.form['nickname_give']
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "username": username_receive,
+        "password": password_hash,
+        "nickname": nickname_receive,
+        "profile_name": username_receive,
+        "profile_pic": "",
+        "profile_pic_real": "profile_pics/profile_placeholder.png",
+        "profile_info": ""
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
+
+
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    # print(value_receive, type_receive, exists)
+    return jsonify({'result': 'success', 'exists': exists})
+
+
+@app.route('/sign_up/check_nick', methods=['POST'])
+def check_nick():
+    nickname_receive = request.form['nickname_give']
+    exists = bool(db.users.find_one({"nickname": nickname_receive}))
+    # print(value_receive, type_receive, exists)
+    return jsonify({'result': 'success', 'exists': exists})
+
+
+@app.route('/main', methods=['GET'])
+def show_cards():
+    capingtems = list(db.savepost.find({}, {'_id': False}))
+    return jsonify({'all_capingtem': capingtems})
+
+
+@app.route("/savepost", methods=["POST"])
+def save_post():
+    img = request.files["file_give"]
+    extension = img.filename.split('.')[-1]
+    today = datetime.now()
+    mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
+    filename = f'img-{mytime}'
+    save_to = f'static/{filename}.{extension}'
+    img.save(save_to)
+    title_receive = request.form["title_give"]
+    category_receive = request.form["category_give"]
+    price_receive = request.form["price_give"]
+    starpoints_receive = request.form["starPoints_give"]
+    comment_receive = request.form["comment_give"]
+
+    post_list = list(db.savepost.find({}, {'_id:False'}))
+    count = len(post_list) + 1
+
+    doc = {
+        'num': count,
+        'img': f'{filename}.{extension}',
+        'title': title_receive,
+        'category': category_receive,
+        'price': price_receive,
+        'starpoints': starpoints_receive,
+        'comment': comment_receive
+    }
+
+    db.savepost.insert_one(doc)
+
+    return jsonify({'msg': '등록 완료!'})
+
+@app.route('/main/<index>', methods=["GET"])
+def detail(index):
+    details = list(db.savepost.find({'num' : int(index)}, {"_id": False}))
+    return render_template("view.html", data=details)
+
+@app.route('/comment', methods=['POST'])
+def posting():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        # 포스팅하기
+        user_info = db.users.find_one({"username": payload["id"]})
+        comment_receive = request.form["comment_give"]
+        date_receive = request.form["date_give"]
+        print(type(date_receive))
+        doc = {
+
+            "comment": comment_receive,
+            "date": date_receive
+        }
+        db.posts.insert_one(doc)
+        return jsonify({"result": "success", 'msg': '포스팅 성공'})
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+@app.route('/comment/<index>', methods=['GET'])
+def get_posts():
+        token_receive = request.cookies.get('mytoken')
+        try:
+            payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+            posts = list(db.posts.find({}).sort("date", -1).limit(10))
+            for post in posts:
+                post["_id"] = str(post["_id"])
+            return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다.", "posts": posts})
+        except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+            return redirect(url_for("home"))
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
